144d1b340af4182bc397b66a0fbdbcd7d2bd606d
waleslau <waleslau@foxmail.com>
2020-07-17 10:07:25 +0800

    Update

---
+++ Linux/docker/mysql-docker.sh
@@ -0,0 +1,1 @@
+docker run --name some-mysql -p 3406:3306 -e MYSQL_ROOT_PASSWORD=000000 -d mysql:5.7
--- Linux/mysql-docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --name some-mysql -p 3406:3306 -e MYSQL_ROOT_PASSWORD=000000 -d mysql:5.7
