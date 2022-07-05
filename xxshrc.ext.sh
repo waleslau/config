81ad9c06cfa1496c62d16f4e3d554e3da908b94c
waleslau <waleslau@foxmail.com>
2022-07-05 23:23:55 +0800

    update

--- xxshrc.ext.sh
+++ xxshrc.ext.sh
@@ -40,4 +40,5 @@ alias hugo-server='hugo server -D'
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias nvm-update='nvm install --lts && nvm use --lts && nvm alias default lts/*'
 
