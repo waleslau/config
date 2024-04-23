60f65857928584b2ea9b6fea36f4c81fbd972652
waleslau <waleslau@foxmail.com>
2024-04-23 23:45:27 +0800

    delete clash

--- clash/update_country_mmdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -O /var/tmp/Country.mmdb https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
-cp -f /var/tmp/Country.mmdb /home/idea/.config/oh-my-clash/Country.mmdb
-echo -e "\nAlready copy to '/home/idea/.config/oh-my-clash/Country.mmdb'\n"
